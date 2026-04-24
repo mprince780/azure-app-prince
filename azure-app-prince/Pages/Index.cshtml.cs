f67c65b2c3d308a58c48fa13d88e0d889a77a220
princem <md.prince@autodesk.com>
2026-04-25 03:34:17 +0530

    Add project files.

---
+++ azure-app-prince/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_prince.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
